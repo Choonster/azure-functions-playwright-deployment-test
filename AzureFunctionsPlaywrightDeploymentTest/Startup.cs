53e319be4cd611aec7b2f1afa27d00addb1bc709
Choonster TheMage <Choonster.2010@gmail.com>
2021-11-21 20:39:08 +1100

    Add Playwright and ls command

---
+++ AzureFunctionsPlaywrightDeploymentTest/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+
+namespace AzureFunctionsPlaywrightDeploymentTest
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            string playwrightMessage;
+            try
+            {
+                Microsoft.Playwright.Program.Main(new[] { "install chromium" });
+                playwrightMessage = "playwright install worked";
+
+            }
+            catch (Microsoft.Playwright.PlaywrightException ex)
+            {
+                playwrightMessage = $"PlaywrightException: {ex.Message}";
+            }
+
+            var appBinFolder = System.IO.Path.GetDirectoryName(typeof(Startup).Assembly.Location);
+
+            using var process = new System.Diagnostics.Process
+            {
+                StartInfo = new()
+                {
+                    FileName = "/bin/bash",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    Arguments = $"-c \"ls -lRaA \\\"{appBinFolder}/..\\\"",
+                },
+            };
+
+            process.Start();
+            process.WaitForExit();
+
+            var stdout = process.StandardOutput.ReadToEnd();
+            var stderr = process.StandardError.ReadToEnd();
+
+            throw new Exception($"\n\nPlaywright: {playwrightMessage}\n\nstdout: {stdout}\n\nstderr: {stderr}");
+        }
+    }
+}
